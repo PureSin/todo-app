04f694c728c99c9f5bc386b09f1d9bcfc447f4e8
Kelvin Ma <kelvin@codecademy.com>
2015-02-25 21:55:19 -0500

    setup initial workspace

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+
+// Load some modules which are installed through NPM.
+var gulp = require('gulp');
+var browserify = require('browserify');  // Bundles JS.
+var reactify = require('reactify');  // Transforms React JSX to JS.
+var source = require('vinyl-source-stream');
+ 
+// Define some paths.
+var paths = {
+  app_js: ['./src/js/app.jsx'],
+  js: ['src/js/*.*'],
+};
+  
+// Our JS task. It will Browserify our code and compile React JSX files.
+gulp.task('js', function() {
+  // Browserify/bundle the JS.
+  browserify(paths.app_js)
+    .transform(reactify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./build/'));
+});
+ 
+// Rerun tasks whenever a file changes.
+gulp.task('watch', function() {
+  gulp.watch(paths.js, ['js']);
+});
+ 
+// The default task (called when we run `gulp` from cli)
+gulp.task('default', ['watch']);
